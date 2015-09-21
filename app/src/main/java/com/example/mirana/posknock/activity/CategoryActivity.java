380bcc448386bc1376dd05dd25fc46a4ce76d24d
Jason Tang <jasontang736@gmail.com>
2015-09-21 22:46:23 +0800

    category_list finished and category2_list is starting..

---
+++ app/src/main/java/com/example/mirana/posknock/activity/CategoryActivity.java
@@ -0,0 +1,17 @@
+package com.example.mirana.posknock.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.mirana.posknock.R;
+
+/**
+ * Created by xiaotingtang on 9/21/15.
+ */
+public class CategoryActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_list);
+    }
+}
--- app/src/main/java/com/example/mirana/posknock/activity/HomeActivity.java
+++ app/src/main/java/com/example/mirana/posknock/activity/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.mirana.posknock.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.mirana.posknock.R;
 
@@ -9,9 +12,18 @@ import com.example.mirana.posknock.R;
  * Created by xiaotingtang on 9/19/15.
  */
 public class HomeActivity extends Activity {
+    private ImageView thumb_machine = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        thumb_machine = (ImageView) findViewById(R.id.thumb_machine);
+        thumb_machine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeActivity.this, CategoryActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
