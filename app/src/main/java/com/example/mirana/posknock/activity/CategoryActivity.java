45d084fc6e7609094114efed1bada58ef6f1b625
Jason Tang <jasontang736@gmail.com>
2015-09-22 23:13:06 +0800

    enable navigation drawer

--- app/src/main/java/com/example/mirana/posknock/activity/CategoryActivity.java
+++ app/src/main/java/com/example/mirana/posknock/activity/CategoryActivity.java
@@ -1,6 +1,7 @@
 package com.example.mirana.posknock.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
@@ -68,22 +69,17 @@ public class CategoryActivity extends Activity {
 
     /** Swaps fragments in the main content view */
     private void selectItem(int position) {
-//        // Create a new fragment and specify the planet to show based on position
-//        Fragment fragment = new PlanetFragment();
-//        Bundle args = new Bundle();
-//        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-//        fragment.setArguments(args);
-//
-//        // Insert the fragment by replacing any existing fragment
-//        FragmentManager fragmentManager = getFragmentManager();
-//        fragmentManager.beginTransaction()
-//                .replace(R.id.content_frame, fragment)
-//                .commit();
-//
-//        // Highlight the selected item, update the title, and close the drawer
-//        mDrawerList.setItemChecked(position, true);
-//        setTitle(mPlanetTitles[position]);
-//        mDrawerLayout.closeDrawer(mDrawerList);
+        Intent i = null;
+        switch (position) {
+            case 0:
+                i = new Intent(CategoryActivity.this, FAQActivity.class);
+                startActivity(i);
+                break;
+            case 1:
+                i = new Intent(CategoryActivity.this, ContactUsActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/mirana/posknock/activity/FAQActivity.java
@@ -0,0 +1,17 @@
+package com.example.mirana.posknock.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.mirana.posknock.R;
+
+/**
+ * Created by xiaotingtang on 9/22/15.
+ */
+public class FAQActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq_page);
+    }
+}
